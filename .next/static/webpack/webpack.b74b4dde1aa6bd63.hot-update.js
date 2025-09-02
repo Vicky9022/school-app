4cdb90fdea9e327cd53e41f5387813a4d77b7b1f
Vicky9022 <vikasrathod90222@gmail.com>
2025-09-02 17:44:55 +0530

    Add port to db.js for Railway MySQL connection

--- .next/static/webpack/webpack.b74b4dde1aa6bd63.hot-update.js
+++ .next/static/webpack/webpack.b74b4dde1aa6bd63.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "73e93b754cdc74ef"; }
+/******/ 	__webpack_require__.h = function() { return "38343a13348064f1"; }
 /******/ }();
 /******/ 
 /******/ }
